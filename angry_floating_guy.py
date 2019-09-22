762daa174d6c39be10fc218427cdbd563a79db95
Edgar Gonçalves <edgar.goncalves@gmail.com>
2019-09-22 18:52:14 +0100

    Sidescrolling of the background image makes it look so much better

--- angry_floating_guy.py
+++ angry_floating_guy.py
@@ -68,25 +68,49 @@ class App:
                                pygame.K_LEFT: False,
                                pygame.K_RIGHT: False}
 
+        self.panning_bg = {'image': None, 'size': (0, 0), 'rect': None,
+                           'screen': None, 'w': 0, 'h': 0, 'x': 0,
+                           'y': 0, 'x1': 0, 'y1': 0, 'x_offset': 0}
+
+        self.the_clock = pygame.time.Clock()
+
     def on_init(self):
         """Loads up variables for the game to start"""
         pygame.init()
         # frequency, size, channels, buffersize
         # pygame.mixer.pre_init(44100, 16, 2, 4096)
-        self._display_surf = pygame.display.set_mode(
-            self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        # self._display_surf = pygame.display.set_mode(
+        #     self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self.image = pygame.transform.scale(
             pygame.image.load("images/emoji.png"), (50, 50))
-        self.background = pygame.transform.scale(
-            pygame.image.load('images/background.png').convert(),
-            (self.weight*2, self.height))
 
+        # Background. Load image, and setup variables for scrolling it
+        background = pygame.transform.scale(
+            pygame.image.load('images/background.png'),
+            (self.weight, self.height))
+
+        self.panning_bg['image'] = background
+        self.panning_bg['size'] = background.get_size()
+        self.panning_bg['rect'] = background.get_rect()
+        self.panning_bg['screen'] = pygame.display.set_mode(
+            self.panning_bg['size'], pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.panning_bg['w'], self.panning_bg['h'] = self.panning_bg['size']
+        # x,y: initial top left corner.
+        self.panning_bg['x'] = 0
+        self.panning_bg['y'] = 0
+
+        # x1,y1: the second image will start to the left of the first
+        # one, creating the illusion of infinite scrolling
+        self.panning_bg['x1'] = -self.panning_bg['w']
+        self.panning_bg['y1'] = 0
+        self.panning_bg['x_offset'] = -5
+
+        # Some music and sound fx
         self.effect = pygame.mixer.Sound('sounds/bounce.wav')
-
         pygame.mixer.music.load('sounds/music.wav')
         pygame.mixer.music.play(-1)
 
-        self.screen = self._display_surf
+        self.screen = self.panning_bg['screen']
         pygame.display.set_caption(
             'Angry Floating Guy! World: {} (w to change world, arrows to move, Esc to quit).'.format(self.current_world.name))
 
@@ -181,9 +205,30 @@ class App:
         #     round(velocity['vert'], 2)))
 
     def on_render(self):
-        # self.screen.fill((100, 150, 150))
         # Render the background
-        self.screen.blit(self.background, (0, 0))
+
+        # Offset the starting points of both copies of the background:
+        self.panning_bg['x1'] += self.panning_bg['x_offset']
+        self.panning_bg['x'] += self.panning_bg['x_offset']
+
+        # Draw the two backgrounds
+        self.screen.blit(self.panning_bg['image'],
+                         (self.panning_bg['x'], self.panning_bg['y']))
+        self.screen.blit(self.panning_bg['image'],
+                         (self.panning_bg['x1'], self.panning_bg['y1']))
+
+        # Rollover background to the right side, depending on which
+        # side we're scrolling.
+        if self.panning_bg['x_offset'] <= 0:
+            if self.panning_bg['x'] < -self.panning_bg['w']:
+                self.panning_bg['x'] = self.panning_bg['w']
+            if self.panning_bg['x1'] < -self.panning_bg['w']:
+                self.panning_bg['x1'] = self.panning_bg['w']
+        else:
+            if self.panning_bg['x'] > self.panning_bg['w']:
+                self.panning_bg['x'] = -self.panning_bg['w']
+            if self.panning_bg['x1'] > self.panning_bg['w']:
+                self.panning_bg['x1'] = -self.panning_bg['w']
 
         # now blit the hero on screen
         self.screen.blit(
@@ -210,3 +255,7 @@ class App:
 if __name__ == "__main__":
     theApp = App()
     theApp.on_execute()
+
+# If running interactively, you will need to run this!
+# theApp = App()
+# theApp.on_execute()
